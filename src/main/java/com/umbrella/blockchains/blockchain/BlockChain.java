448b0a64c7ffcdb3bd14f8af6e287505a1e90b7a
Krig Raseri <krigraseri@gmail.com>
2023-05-23 04:12:05 -0500

    Update BlockChain.java

--- src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
+++ src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
@@ -2,20 +2,17 @@ package com.umbrella.blockchains.blockchain;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 public class BlockChain {
 
     public List<Block> createBlocks() {
         List<Block> blockChain = new ArrayList<>();
-        Block block;
-        String hashOfPrevBlock = "0";
+        Block block = new Block(null);
 
-        for (int i = 1; i <= 5; i++) {
-            block = new Block(i, new Date().getTime(), hashOfPrevBlock);
+        for (int i = 0; i < 5; i++) {
             blockChain.add(block);
-            hashOfPrevBlock = block.getCurrHash();
+            block = new Block(block);
         }
         return Collections.unmodifiableList(blockChain);
     }
