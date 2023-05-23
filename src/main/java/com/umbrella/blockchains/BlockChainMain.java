b568610339910bc2d67ec8ce66b92b8e9b2d88fd
Krig Raseri <krigraseri@gmail.com>
2023-05-23 04:12:07 -0500

    Update BlockChainMain.java

--- src/main/java/com/umbrella/blockchains/BlockChainMain.java
+++ src/main/java/com/umbrella/blockchains/BlockChainMain.java
@@ -3,13 +3,9 @@ package com.umbrella.blockchains;
 import com.umbrella.blockchains.blockchain.Block;
 import com.umbrella.blockchains.blockchain.BlockChain;
 
-import java.util.List;
-
-
 public class BlockChainMain {
     public static void main(String[] args) {
         BlockChain blockChain = new BlockChain();
-        List<Block> blockChainList = blockChain.createBlocks();
-        blockChainList.stream().map(Block::toString).forEach(System.out::println);
+        blockChain.createBlocks().stream().map(Block::toString).forEach(System.out::println);
     }
 }
