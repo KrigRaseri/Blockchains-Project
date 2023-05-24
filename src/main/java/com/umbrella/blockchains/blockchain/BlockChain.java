64b2e4b5296f4719a308f8e77085682e6c64faa8
Krig Raseri <krigraseri@gmail.com>
2023-05-24 05:58:29 -0500

    Update BlockChain.java

--- src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
+++ src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
@@ -3,17 +3,30 @@ package com.umbrella.blockchains.blockchain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class BlockChain {
 
     public List<Block> createBlocks() {
-        List<Block> blockChain = new ArrayList<>();
-        Block block = new Block(null);
+        try (Scanner sc = new Scanner(System.in)) {
+            List<Block> blockChain = new ArrayList<>();
+            Block block = new Block(null);
+            String input = "0".repeat(sc.nextInt());
 
-        for (int i = 0; i < 5; i++) {
-            blockChain.add(block);
-            block = new Block(block);
+            for (int i = 0; i < 5; i++) {
+                long start = System.currentTimeMillis();
+                while (!block.getCurrHash().startsWith(input)) {
+                    block.setMagicNumber(ThreadLocalRandom.current().nextInt(100, 9999998 + 1));
+                    block.setCurrHash(BlockChainUtil.applySha256(BlockChainUtil.createHashString(block)));
+                }
+                long end = System.currentTimeMillis();
+
+                block.setTimeTaken((int) ((end-start) / 1000.0));
+                blockChain.add(block);
+                block = new Block(block);
+            }
+            return Collections.unmodifiableList(blockChain);
         }
-        return Collections.unmodifiableList(blockChain);
     }
 }
