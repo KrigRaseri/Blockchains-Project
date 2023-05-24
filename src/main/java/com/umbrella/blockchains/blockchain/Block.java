fc3c92e06f733d95e5b48d77ad7fabf704d51f5b
Krig Raseri <krigraseri@gmail.com>
2023-05-24 00:22:38 -0500

    Update Block.java

--- src/main/java/com/umbrella/blockchains/blockchain/Block.java
+++ src/main/java/com/umbrella/blockchains/blockchain/Block.java
@@ -8,12 +8,14 @@ import java.util.Date;
 public class Block {
     private int id;
     private long timeStamp;
+    private int magicNumber;
     private String prevHash;
     private String currHash;
 
     public Block(Block previousBlock) {
         this.id = previousBlock == null ? 1 : previousBlock.getId() + 1;
         this.timeStamp = new Date().getTime();
+        this.magicNumber = 0;
         this.prevHash = previousBlock == null ? "0" : previousBlock.getCurrHash();
         this.currHash = BlockChainUtil.applySha256(prevHash + timeStamp + id);;
     }
@@ -24,10 +26,11 @@ public class Block {
                 Block:
                 Id: %d
                 Timestamp: %s
+                Magic number: %s
                 Hash of the previous block:
                 %s
                 Hash of the block:
                 %s
-                """, id,timeStamp,prevHash,currHash);
+                """, id,timeStamp, magicNumber, prevHash,currHash);
     }
 }
