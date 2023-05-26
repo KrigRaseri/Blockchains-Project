94a3b6e697660b069d039150abbc5f718ad90734
Krig Raseri <krigraseri@gmail.com>
2023-05-26 05:50:05 -0500

    Update BlockChain.java
    Adds method adjustNumOfZeros tha will change the number of zero's that the hash should start with based on how long the previous block took.

--- src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
+++ src/main/java/com/umbrella/blockchains/blockchain/BlockChain.java
@@ -2,9 +2,12 @@ package com.umbrella.blockchains.blockchain;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class BlockChain {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     public static ArrayList<Block> createBlockChain() {
         try (Scanner sc = new Scanner(System.in)) {
@@ -15,6 +18,7 @@ public class BlockChain {
             Block block = new Block(null);
             for (int i = 0; i < 5; i++) {
                 blockChain.add(generateProvedBlock(block, input));
+                input = adjustNumOfZeros(input.length(), block.getTimeTaken());
                 block = new Block(block);
             }
             return blockChain;
@@ -30,5 +34,14 @@ public class BlockChain {
         long end = System.currentTimeMillis();
         block.setTimeTaken((int) ((end-start) / 1000.0));
         return block;
+    }
+
+    public static String adjustNumOfZeros(int numOfZeros, int timeTaken) {
+        if (timeTaken <= 10) {
+            return "0".repeat(numOfZeros + 1);
+        } else if (timeTaken >= 60) {
+            return "0".repeat(numOfZeros - 1);
+    }
+        return "0".repeat(numOfZeros);
     }
 }
