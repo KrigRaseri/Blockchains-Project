bc0b466da0d6321218d7df8f862d3c4bf287b7b6
Krig Raseri <krigraseri@gmail.com>
2023-05-25 21:32:23 -0500

    Update BlockChainMain.java

--- src/main/java/com/umbrella/blockchains/BlockChainMain.java
+++ src/main/java/com/umbrella/blockchains/BlockChainMain.java
@@ -1,10 +1,21 @@
 package com.umbrella.blockchains;
 
+import com.esotericsoftware.kryo.Kryo;
 import com.umbrella.blockchains.blockchain.Block;
 import com.umbrella.blockchains.blockchain.BlockChain;
+import com.umbrella.blockchains.kryo.KryoImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class BlockChainMain {
     public static void main(String[] args) {
-        BlockChain.createBlockChain().stream().map(Block::toString).forEach(System.out::println);
+        ArrayList<Block> blockChain = BlockChain.createBlockChain();
+        blockChain.stream().map(Block::toString).forEach(System.out::println);
+
+        Kryo kryo = KryoImpl.runKryo();
+        blockChain.forEach(b -> KryoImpl.writeKryo(kryo, blockChain));
+        KryoImpl.readKryo(kryo).forEach(System.out::println);
     }
 }
