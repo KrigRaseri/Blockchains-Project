77cca23276dbef792ee4494478ea8a09d753c5a8
Krig Raseri <krigraseri@gmail.com>
2023-05-29 03:57:33 -0500

    Update BlockChainUtil.java

--- src/main/java/com/umbrella/blockchains/blockchain/BlockChainUtil.java
+++ src/main/java/com/umbrella/blockchains/blockchain/BlockChainUtil.java
@@ -1,9 +1,12 @@
 package com.umbrella.blockchains.blockchain;
 
 import java.security.MessageDigest;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public interface BlockChainUtil {
-
     static String applySha256(String input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -27,5 +30,38 @@ public interface BlockChainUtil {
                 + block.getTimeStamp()
                 + block.getMagicNumber()
                 + block.getPrevHash();
+    }
+
+    static String adjustNumOfZeros(int numOfZeros, int timeTaken) {
+        if (timeTaken <= 10) {
+            return "0".repeat(numOfZeros + 1);
+        } else if (timeTaken >= 60) {
+            return "0".repeat(numOfZeros - 1);
+        }
+        return "0".repeat(numOfZeros);
+    }
+
+    static List<Callable<Block>> createCallableList(Block block, String numOfZeros) {
+        return IntStream.range(0, 20)
+                .mapToObj(j -> newCallable(block, numOfZeros))
+                .collect(Collectors.toList());
+    }
+
+    private static Callable<Block> newCallable(Block block, String numOfZeros) {
+        return new Callable<Block>() {
+            @Override
+            public Block call() throws Exception {
+                return BlockChain.generateProvedBlock(block, numOfZeros);
+            }
+        };
+    }
+
+    static Block copyBlock(Block block) {
+        Block blockCopy = new Block();
+        blockCopy.setCurrHash(block.getCurrHash());
+        blockCopy.setId(block.getId());
+        blockCopy.setPrevHash(block.getPrevHash());
+        blockCopy.setTimeStamp(block.getTimeStamp());
+        return blockCopy;
     }
 }
