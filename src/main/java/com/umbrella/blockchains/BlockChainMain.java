dc5c530ee0187bb35986214af3a174ed760565d2
Krig Raseri <krigraseri@gmail.com>
2023-05-30 22:54:36 -0500

    Update BlockChainMain.java

--- src/main/java/com/umbrella/blockchains/BlockChainMain.java
+++ src/main/java/com/umbrella/blockchains/BlockChainMain.java
@@ -13,13 +13,12 @@ import java.util.List;
  It provides the entry point for the application and contains the main method to start the execution.
  The commented out portion can be uncommented for the use of Kryo.
 
- 1. The number of leading zero's to find in a hash is chosen by the user.
- 2. A block is created and tasks that change the hash are submitted to the ExecutionService until the zeros are found.
- 3. Once found the block will be set with the new hash, magic num, time taken, and thread number that found the result.
- 4. The amount of zeros will then be shifted depending on time taken. 10 sec or less it goes up, 60+ it goes up.
- 5. The block is added to an array list.
- 6. Finally, once done creating blocks the array list is returned, and each block is printed out.
- 7. If kryo is enabled then it will be serialized to data.bin.
+ 1. A block is created and tasks that change the hash are submitted to the ExecutionService until the zeros are found.
+ 2. Once found the block will be set with the new hash, magic num, time taken, and thread number that found the result.
+ 3. The amount of zeros will then be shifted depending on time taken. 10 sec or less it goes up, 60+ it goes up.
+ 4. The block is added to an array list.
+ 5. Finally, once done creating blocks the array list is returned, and each block is printed out.
+ 6. If kryo is enabled then it will be serialized to data.bin.
  */
 public class BlockChainMain {
     public static void main(String[] args) {
