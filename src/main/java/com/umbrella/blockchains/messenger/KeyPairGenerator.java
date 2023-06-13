869cc630f896a61695f67cd6f2b149ada7eab825
Krig Raseri <krigraseri@gmail.com>
2023-06-13 06:08:24 -0500

    Create KeyPairGenerator.java

---
+++ src/main/java/com/umbrella/blockchains/messenger/KeyPairGenerator.java
@@ -0,0 +1,12 @@
+package com.umbrella.blockchains.messenger;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class KeyPairGenerator {
+    private KeyPairGenerator keyGen;
+    private KeyPair pair;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+}
