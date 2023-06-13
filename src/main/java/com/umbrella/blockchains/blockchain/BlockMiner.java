ebbfb1b8b2386612a3f6ab0e7dd7f3de41a0ecd1
Krig Raseri <krigraseri@gmail.com>
2023-06-12 22:23:09 -0500

    Create BlockMiner.java
    Handles mining, in this case it's the generateProvedBlock method now named mineProvedBlock.

---
+++ src/main/java/com/umbrella/blockchains/blockchain/BlockMiner.java
@@ -0,0 +1,37 @@
+package com.umbrella.blockchains.blockchain;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.umbrella.blockchains.blockchain.BlockChainUtil.createNewHash;
+
+public class BlockMiner {
+
+    /**
+     * Mines for a proved block by finding a hash with the desired number of leading zeros.
+     *
+     * @param block        The previous block in the blockchain.
+     * @param leadingZeros The desired number of leading zeros.
+     * @return The proved block.
+     * @throws InterruptedException If the thread is interrupted.
+     */
+    protected Block mineProvedBlock(Block block, String leadingZeros) throws InterruptedException {
+        Block blockCopy = BlockChainUtil.copyBlock(block);
+
+        long start = System.currentTimeMillis();
+        while (!blockCopy.getCurrHash().startsWith(leadingZeros)) {
+            int randomMagicNum = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+            blockCopy.setMagicNumber(randomMagicNum);
+            blockCopy.setCurrHash(createNewHash(blockCopy));
+
+            if (Thread.currentThread().isInterrupted()) {
+                System.out.println("Block mining was interrupted.");
+                throw new InterruptedException();
+            }
+        }
+        long end = System.currentTimeMillis();
+
+        blockCopy.setTimeTaken((int) ((end - start) / 1000.0));
+        blockCopy.setMinerNum(Thread.currentThread().getId());
+        return blockCopy;
+    }
+}
