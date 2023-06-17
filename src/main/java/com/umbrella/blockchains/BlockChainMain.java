81453d800a59fcec45894f0a5a01197d23f795c2
Krig Raseri <krigraseri@gmail.com>
2023-06-16 22:22:31 -0500

    Update BlockChainMain.java

--- src/main/java/com/umbrella/blockchains/BlockChainMain.java
+++ src/main/java/com/umbrella/blockchains/BlockChainMain.java
@@ -6,7 +6,9 @@ import com.google.inject.Injector;
 import com.umbrella.blockchains.blockchain.Block;
 import com.umbrella.blockchains.blockchain.BlockChain;
 import com.umbrella.blockchains.kryo.KryoImpl;
+import com.umbrella.blockchains.messenger.GenerateKeys;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
@@ -15,17 +17,38 @@ import java.util.List;
  It provides the entry point for the application and contains the main method to start the execution.
  The commented out portion can be uncommented for the use of Kryo.
 
- 1. A block is created and tasks that change the hash are submitted to the ExecutionService until the zeros are found.
- 2. Once found the block will be set with the new hash, magic num, time taken, and thread number that found the result.
- 3. The amount of zeros will then be shifted depending on time taken. 10 sec or less it goes up, 60+ it goes up.
- 4. The block is added to an array list.
- 5. Finally, once done creating blocks the array list is returned, and each block is printed out.
- 6. If kryo is enabled then it will be serialized to data.bin.
+ 1. If not already, a public/private keyPair will be created in resources/KeyPair.
+ 2. A block is created and tasks that change the hash are submitted to the ExecutionService until the zeros are found.
+ 3. While mining random pre-made messages will be chosen then encrypted and "sent" with a signature. For the sake of demo
+    it is written to resources/MyData/SignedData.txt.
+ 4. The message will be decrypted and the signature validated. If true it will be added to the chat logs.
+ 5. Once found the block will be set with the new hash, magic num, time taken, the thread number that found the result,
+    and the entire chat logs that were recorded while mining.
+ 6. The amount of zeros will then be shifted depending on time taken. 10 sec or less it goes up, 60+ it goes up.
+ 7. The block is added to an array list.
+ 8. Finally, once done creating blocks the array list is returned, and each block is printed out.
+ 9. If kryo is enabled then it will be serialized to data.bin.
  */
 public class BlockChainMain {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector();
         BlockChain blockChain = injector.getInstance(BlockChain.class);
+
+        GenerateKeys generateKeys;
+
+        try {
+            generateKeys = new GenerateKeys(1024);
+            generateKeys.createKeys();
+
+            generateKeys.writeKeysToFile("./src/main/resources/KeyPair/publicKey",
+                    generateKeys.getPublicKey().getEncoded());
+
+            generateKeys.writeKeysToFile("./src/main/resources/KeyPair/privateKey",
+                    generateKeys.getPrivateKey().getEncoded());
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
 
         blockChain.createBlockChain().stream().map(Block::toString).forEach(System.out::println);
 
